b7d8211c2aaae85bfaaa2a610b013b0deb2e9f69
thealienthing <benjamin.stoneking@gmail.com>
2019-03-21 13:16:29 -0600

    Knob placement. Now adding text labels

--- Source/ControllersPanel.h
+++ Source/ControllersPanel.h
@@ -20,19 +20,26 @@ class ControllersPanel    : public Component
 public:
     ControllersPanel()
     {
-        setSize(200, 350);
-        width = 200;
-        height = 350;
+        setSize(width, height);
+        
         tune.setSliderStyle(Slider::SliderStyle::Rotary);
-        tune.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, false, 20, 5);
+        tune.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, false, 0, 0);
         glide.setSliderStyle(Slider::SliderStyle::Rotary);
-        glide.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, false, 40, 10);
+        glide.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, false, 0, 0);
         modulationMix.setSliderStyle(Slider::SliderStyle::Rotary);
-        modulationMix.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, false, 40, 10);
+        modulationMix.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, false, 0, 0);
+        
+        knobFont.setTypefaceName("Futura");
+        knobFont.setHeight(16.0f);
+        panelFont.setTypefaceName("Futura");
+        panelFont.setHeight(16.0f);
         
         addAndMakeVisible(tune);
         addAndMakeVisible(glide);
         addAndMakeVisible(modulationMix);
+        
+        addAndMakeVisible(glideLabel);
+        addAndMakeVisible(modMixLabel);
     }
 
     ~ControllersPanel()
@@ -47,13 +54,15 @@ public:
     void paint (Graphics& g) override
     {
         area = Rectangle<int>(0, 0, 200, 350);
-        g.setColour(Colours::firebrick);
-        g.fillRect(area);
-        g.setFont(Font(16.0f));
         g.setColour(Colours::black);
-        g.drawText("Controllers Panel", area, Justification::centredBottom, true);
+        g.fillRect(area);
         
+        g.setFont(panelFont);
         g.setColour(Colours::white);
+        g.drawText("CONTROLLERS", area, Justification::centredBottom, true);
+        
+        g.drawText("TUNE", (, tune.getY()+50, 40, 10, Justification::horizontallyCentred);
+        
         g.drawLine(0, 0, width, 0);
         g.drawLine(width, 0, width, height);
         g.drawLine(width, height, 0, height);
@@ -72,19 +81,26 @@ public:
         bounds = getLocalBounds();
         bounds.removeFromBottom(bounds.getHeight()*2/5);
         int section = bounds.getWidth()/4;
-        bounds.removeFromLeft(section);
         bounds.removeFromRight(section);
+        bounds.removeFromLeft(section);
         tune.setBounds(bounds);
+        
     }
 
 private:
     Rectangle<int> area;
-    int height;
-    int width;
+    int height = 350;
+    int width = 200;
     int knobBorder = 5;
+    
+    Font panelFont;
+    Font knobFont;
     
     Slider tune;
     Slider glide;
     Slider modulationMix;
+    
+    Label glideLabel;
+    Label modMixLabel;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ControllersPanel)
 };
